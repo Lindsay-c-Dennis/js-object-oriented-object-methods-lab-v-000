5cd98d61eeba5b4f3d4fee8e648b0d3ef2275f66
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-24 17:37:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function  BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homestate;
   this.training = training;
-  this.veto = function
+  this.veto = function() {
+    console.log('No, I must disagree');
+  };
 }
